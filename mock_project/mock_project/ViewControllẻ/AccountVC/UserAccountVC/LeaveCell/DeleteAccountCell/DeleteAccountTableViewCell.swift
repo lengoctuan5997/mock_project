d63cd7dc8a45b0fb25f939dd8f679ddbaa3d8142
Charon116 <lcquang116@gmail.com>
2022-10-14 09:48:50 +0700

    update delete account

---
+++ mock_project/mock_project/ViewControllẻ/AccountVC/UserAccountVC/LeaveCell/DeleteAccountCell/DeleteAccountTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DeleteAccountTableViewCell.swift
+//  mock_project
+//
+//  Created by Le on 13/10/2022.
+//
+
+import UIKit
+
+class DeleteAccountTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
